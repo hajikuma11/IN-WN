c4a738b48135e4e423effca5f3df62203e04529a
hajikuma11 <hajimekuma11@gmail.com>
2020-04-26 19:12:58 +0900

    タイトルと最終更新日時のセル位置をグローバル変数として定義するように変更

--- sheet.js
+++ sheet.js
@@ -1,17 +1,19 @@
 const spreadsheet = SpreadsheetApp.openById('1mhhPptM-czxLqlbdJ0qEnsLNEMFdgAGOAdxRkQUq3WE');
 const title = spreadsheet.getSheetByName('title');
+const lastModCell = "B1";
+const titleCell = "C1";
 
 function setData(range,data) {
   title.getRange(range).setValue(data);
 }
 
 function setTitleData(dataObj) {
-  setData("A1",dataObj["lastModified"]);
-  setData("B1",dataObj["title"]);
+  setData(lastModCell,dataObj["lastModified"]);
+  setData(titleCell,dataObj["title"]);
 }
 
 function compare(dataObj) {
-  const titleData = title.getRange("B1").getValues();
+  const titleData = title.getRange(titleCell).getValues();
   const sheetTitle = titleData[0].toString();
 
   return sheetTitle == dataObj["title"];
